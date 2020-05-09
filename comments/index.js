5afb5914653f9fb89f2880015cd2861752cc7ad5
gabecadiz <gabecadiz@hotmail.com>
2020-05-09 15:17:35 -0400

    initial react page setup, connect to posts service

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors());
+app.options('*', cors());
 
 const commentsByPostId = {};
 
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // temporary database for toy project
 const posts = {};
